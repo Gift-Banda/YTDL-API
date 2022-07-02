02485848ebd030bed2ce7a6d59dc65f9d38f5178
Gift-Banda <bandaolga19@gmail.com>
2022-07-02 15:30:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# express-simple-zbzyhj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/express-simple-zbzyhj)
---
+++ index.js
@@ -0,0 +1,51 @@
+// Require stuff
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+const { getInfo } = require('ytdl-getinfo');
+
+// Cors
+app.use(cors());
+
+// Listen on port 3000
+app.listen(3300, () => {
+  console.log('Server running on port 3000');
+});
+
+// Download video
+app.get('/d', (req, res) => {
+  let URL = req.query.url;
+  let FORMAT = req.query.format;
+  let QUALITY = req.query.quality;
+  let VIDEO_NAME;
+
+  getInfo(`${URL}`).then((info) => {
+    // info.items[0] should contain the output of youtube-dl --dump-json
+    VIDEO_NAME = info.items[0].title;
+    res.header(
+      'Content-Disposition',
+      `attachment; filename="${VIDEO_NAME}.${FORMAT || 'mp4'}"`
+    );
+
+    try {
+      if (QUALITY === 'highest') {
+        console.log('Downloading highest format');
+        ytdl(URL, {
+          format: FORMAT.toLowerCase() || 'mp4',
+        }).pipe(res);
+      } else {
+        console.log('Downloading custom format');
+        ytdl(URL, {
+          format: FORMAT.toLowerCase() || 'mp4',
+          quality: QUALITY || 'highest',
+        }).pipe(res);
+      }
+    } catch (err) {
+      console.log('There was an error, attempting to download still.');
+      ytdl(URL, {
+        format: 'mp4',
+      }).pipe(res);
+    }
+  });
+});
